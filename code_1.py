4f1358b1635b2f55e24c8dbd2ec1e4e529023ed6
lepikakut <152447835+lepikakut@users.noreply.github.com>
2023-12-20 10:36:03 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,28 @@
+        int[] data = new int[10];
+
+        for (int item : data) {
+
+        }
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+}
+    }
+        }    data.forEach(item => {
+    const data = [];
+    });
+        console.log("Random Number: " + item);
+
+}
+main();
+    }
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+}
+    return data;
+function main() {
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
